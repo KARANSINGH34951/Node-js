a4dc76ecc99f7a581c2cab006f9516d68842a858
karan singh D <ceitkaransingh2026@gmail.com>
2024-05-07 21:16:36 +0530

    starting with node js

---
+++ file.js
@@ -0,0 +1,29 @@
+const fs=require("fs")
+//sync
+// fs.writeFileSync("./test.txt","file created");
+
+// async
+// fs.writeFile("./test/txt","hello world async",(err))
+
+
+// const result=fs.readFileSync("./contacts.txt","utf-8")
+// console.log(result);
+
+// fs.readFile("./contacts.txt","utf-8",(err,result)=>{
+//   if(err){
+//     console.log("error",err);
+//   }
+//   else{
+//     console.log(result);
+//   }
+// })
+
+//  aync does not retur anything and have extra callback in it
+
+
+// fs.appendFileSync("./test.txt","login \n");
+// fs.cpSync("./test.txt","./copy.txt"); copy the file
+// fs.unlinkSync("./copy.txt") its delete the coped filr
+
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const math = require('./Math.js'); 
+
+console.log("hello world");
+console.log(math.add(1, 2)); // Path: index.js
+// Path: index.html
+
